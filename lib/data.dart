4174597db124bcebeb05dcfba0444d914c1a093f
zhuohoudeputao <linzuxuan@outlook.com>
2020-07-13 13:24:52 +0800

    add sqflite package

--- lib/data.dart
+++ lib/data.dart
@@ -1,10 +1,12 @@
 /*
  * @Author: zhuohoudeputao
  * @LastEditors: zhuohoudeputao
- * @LastEditTime: 2020-07-11 10:47:54
+ * @LastEditTime: 2020-07-13 00:31:26
  * @Description: file content
- */ 
+ */
 
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
 
 List<Widget> infoList = <Widget>[];
+
