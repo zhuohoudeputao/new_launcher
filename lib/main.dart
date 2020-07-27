dd8c7ff323c34e4c028af26b7d8b44769f5b5d0c
zhuohoudeputao <linzuxuan@outlook.com>
2020-07-27 22:21:41 +0800

    return check and focus reset

--- lib/main.dart
+++ lib/main.dart
@@ -49,76 +49,92 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  DateTime _lastReturn; // last return time
   @override
   Widget build(BuildContext context) {
     List<Widget> infoList = context.watch<InfoModel>().infoList;
     List<Widget> suggestList = context.watch<ActionModel>().suggestList;
-    return Stack(fit: StackFit.expand, children: <Widget>[
-      // Background Image
-      Consumer<BackgroundImageModel>(
-          builder: (context, BackgroundImageModel background, child) {
-        return Image(
-            image: context.watch<BackgroundImageModel>().backgroundImage,
-            fit: BoxFit.cover);
-      }),
-      // Main Scaffold
-      Scaffold(
-        backgroundColor: Colors.transparent,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          verticalDirection: VerticalDirection.up,
-          children: <Widget>[
-            // Input Box
-            Card(
-              child: TextField(
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(
-                    hintText: ">_ Input something here.",
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                          color: Theme.of(context).accentColor, width: 10.0),
-                    )),
-                controller: context.watch<ActionModel>().inputBoxController,
-                onEditingComplete: context.watch<ActionModel>().runFirstAction,
-                onChanged: context.watch<ActionModel>().generateSuggestList,
+    return WillPopScope(
+      onWillPop: () async {
+        if (_lastReturn == null ||
+            DateTime.now().difference(_lastReturn) > Duration(seconds: 1)) {
+          _lastReturn = DateTime.now();
+          return false;
+        }
+        return true;
+      }, // return check
+      child: Stack(fit: StackFit.expand, children: <Widget>[
+        // Background Image
+        Consumer<BackgroundImageModel>(
+            builder: (context, BackgroundImageModel background, child) {
+          return Image(
+              image: context.watch<BackgroundImageModel>().backgroundImage,
+              fit: BoxFit.cover);
+        }),
+        // Main Scaffold
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            verticalDirection: VerticalDirection.up,
+            children: <Widget>[
+              // Input Box
+              Card(
+                child: TextField(
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                      hintText: ">_ Input something here.",
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Theme.of(context).accentColor, width: 10.0),
+                      )),
+                  controller: context.watch<ActionModel>().inputBoxController,
+                  onEditingComplete:
+                      context.watch<ActionModel>().runFirstAction,
+                  onChanged: context.watch<ActionModel>().generateSuggestList,
+                ),
               ),
-            ),
-            // Suggestion Area
-            Container(
-              height: 50.0,
-              child: ListView.builder(
-                // suggestion displayer
-                itemCount: suggestList.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Selector<ActionModel, Widget>(
-                    selector: (context, provider) => suggestList[index],
-                    builder: (context, value, child) => suggestList[index],
-                  );
+              // Suggestion Area
+              Container(
+                height: 50.0,
+                child: ListView.builder(
+                  // suggestion displayer
+                  itemCount: suggestList.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Selector<ActionModel, Widget>(
+                      selector: (context, provider) => suggestList[index],
+                      builder: (context, value, child) => suggestList[index],
+                    );
+                  },
+                  scrollDirection: Axis.horizontal,
+                ),
+              ),
+              // Information Area
+              Expanded(
+                  child: GestureDetector(
+                onTap: () { // put away the keyboard
+                  FocusScope.of(context).requestFocus(FocusNode());
                 },
-                scrollDirection: Axis.horizontal,
-              ),
-            ),
-            // Information Area
-            Expanded(
-              child: ListView.builder(
-                // infomation displayer
-                itemCount: infoList.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Selector<InfoModel, Widget>(
-                    selector: (context, provider) =>
-                        infoList[infoList.length - index - 1],
-                    builder: (context, value, child) =>
-                        infoList[infoList.length - index - 1],
-                  );
-                },
-                scrollDirection: Axis.vertical,
-                reverse: true, // reverse the entire infoList and the index
-              ),
-            ),
-          ],
+                child: ListView.builder(
+                  // infomation displayer
+                  itemCount: infoList.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Selector<InfoModel, Widget>(
+                      selector: (context, provider) =>
+                          infoList[infoList.length - index - 1],
+                      builder: (context, value, child) =>
+                          infoList[infoList.length - index - 1],
+                    );
+                  },
+                  scrollDirection: Axis.vertical,
+                  reverse: true, // reverse the entire infoList and the index
+                ),
+              )),
+            ],
+          ),
         ),
-      ),
-    ]);
+      ]),
+    );
   }
 }
